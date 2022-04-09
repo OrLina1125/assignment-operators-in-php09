4021a9c768d8b7690a421cc15c441159c49b6b68
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-09 11:42:34 +0900

    add assignment operators

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+
+<?php
+    
+    // Assignment Operators
+    $x = 100;
+    // $x = $x + 20;
+    // $x += 20;
+    $x /= 20;
+
+
+    echo $x;
+?>
+</body>
+</html>
